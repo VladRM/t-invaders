7740f18b0b564ca26b8e15785650a659e28b36ab
Vlad Rosca <vlad.rosca@flowberry.tech>
2024-11-08 20:52:49 +0200

    fix: Update player sprite path to correct assets directory

--- game.js
+++ game.js
@@ -31,7 +31,7 @@ class GameScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('player', 'path/to/player/sprite.png');
+        this.load.image('player', 'assets/img/player.png');
     }
 
     create() {
